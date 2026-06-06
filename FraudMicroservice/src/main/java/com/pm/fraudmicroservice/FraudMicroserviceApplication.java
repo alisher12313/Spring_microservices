906deed20ea2a98138c72b894c3ff2b63cff8213
Alisher <alibobaladno@gmail.com>
2026-06-06 18:24:49 +0500

    FraudMicroservice project init

---
+++ FraudMicroservice/src/main/java/com/pm/fraudmicroservice/FraudMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pm.fraudmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FraudMicroserviceApplication.class, args);
+    }
+
+}
